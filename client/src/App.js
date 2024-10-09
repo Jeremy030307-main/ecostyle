f141d4e20111a41cf0a5669607bfc454792782a5
Teng Kong Cheng <kten0008@student.monash.edu>
2024-10-09 23:57:23 +0800

    Setup basic routing for home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Shop from './Pages/Shop';
+import EcoPoint from './Pages/EcoPoint';
+import About from './Pages/About';
+import Cart from './Pages/Cart';
+import Wishlist from './Pages/Wishlist'; 
+import Accout from './Pages/Accout';
+import Product from './Pages/Product';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+
+      <Routes>
+        <Route path='/' element={<Shop/>}></Route>
+        <Route path='/eco-point' element={<EcoPoint/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/wishlist' element={<Wishlist/>}></Route>
+        <Route path='/cart' element={<Cart/>}></Route>
+        <Route path='/account' element={<Accout/>}></Route>
+        <Route path='/account' element={<Product/>}>
+          <Route path=':productID' element={<Product/>}></Route>
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,13 @@ import logo_text from '../Assets/logo_text.png'
 import cart_icon from '../Assets/cart.svg'
 import wishlist_icon from '../Assets/wishlist.svg'
 import account_icon from '../Assets/account.svg'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+const NoDecorationLink = styled(Link)`
+  text-decoration: none;
+  color: #626262; /* Change this to your desired color */
+`;
 
 const Navbar = () => {
 
@@ -12,20 +18,20 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className='nav-logo'>
-                <img src={logo_text} alt=''/>
+                <Link to='/'><img src={logo_text} alt=''/></Link>
             </div>
 
             <ul className='nav-menu'>
-                 <li onClick={() => {setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
-                 <li onClick={() => {setMenu("eco-point")}}>EcoPoint{menu==="eco-point"?<hr/>:<></>}</li>
-                 <li onClick={() => {setMenu("about")}}>About{menu==="about"?<hr/>:<></>}</li>
+                 <li onClick={() => {setMenu("shop")}}><NoDecorationLink style={{textDecoration: 'none'}} to='/'>Shop</NoDecorationLink>{menu==="shop"?<hr/>:<></>}</li>
+                 <li onClick={() => {setMenu("eco-point")}}><NoDecorationLink style={{textDecoration: 'none'}} to='/eco-point'>EcoPoint</NoDecorationLink>{menu==="eco-point"?<hr/>:<></>}</li>
+                 <li onClick={() => {setMenu("about")}}><NoDecorationLink style={{textDecoration: 'none'}} to='/about'>About</NoDecorationLink>{menu==="about"?<hr/>:<></>}</li>
             </ul>
 
             <div className='nav-account-cart-wishlist'>
-                <img src={wishlist_icon} alt="" />
-                <img src={cart_icon} alt="" />
+                <Link to='/wishlist'><img src={wishlist_icon} alt="" /></Link>
+                <Link to='/cart'><img src={cart_icon} alt="" /></Link>
                 <div className='nav-cart-count'>0</div>
-                <img src={account_icon} alt="" />
+                <Link to='/account'><img src={account_icon} alt="" /></Link>
             </div>
         </div>
     );
