78aef08139b1fd85dcef66cc18529222be19b427
Bryan Hii <bhii0003@student.monash.edu>
2024-12-12 18:49:40 +0800

    Created Admin Add Product Page Logic

--- client/src/admin/admin.jsx
+++ client/src/admin/admin.jsx
@@ -1,8 +1,6 @@
-import { Outlet } from "react-router-dom"
 import Navbar from "./Components/Navbar/Navbar";
 import Sidebar from "./Components/Sidebar/Sidebar";
-
-
+import { Outlet } from "react-router-dom"
 
 const Admin = () => {
   return (
