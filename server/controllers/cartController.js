339a1ed902a002245d76eaa3a798a251dd22115f
Teng Kong Cheng <kten0008@student.monash.edu>
2024-12-25 17:16:37 +0800

    Add required file

---
+++ server/controllers/cartController.js
@@ -0,0 +1,22 @@
+import { db } from "../firebase";
+
+export const addProductToCart = async (req ,res) => {
+
+};
+
+export const getUserCart = async (req, res) => {
+
+};
+
+export const updateCartProductQuantity = async (req ,res) => {
+
+};
+
+export const removeCartProduct = async (req ,res) => {
+
+};
+
+export const clearCart = async (req ,res) => {
+
+};
+
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,19 +1,17 @@
 import express from 'express';
-import { authenticate, isAdmin } from './middleware.js';
+import { authenticate, isAdmin, validateRequest } from './middleware.js';
 
-import { newUser, updateUser, deleteUser, getUser, setAdmin } from '../controllers/userController.js';
+import { newUser, updateUser, deleteUser, getUser, setAdmin, getAllUser, listAllUsers } from '../controllers/userController.js';
+import { newUserSchema, updateUserSchema } from '../schema/userSchema.js';
 
 const publicUserRouter = express.Router();
 const adminUserRouter = express.Router();
 
 publicUserRouter.get('/:id', getUser)
 
-publicUserRouter.post('/:id',authenticate, newUser)
+publicUserRouter.post('/', authenticate, newUser)
 
-adminUserRouter.patch('/:id/set-admin', setAdmin)
-
-publicUserRouter.patch('/:id',authenticate, updateUser)
-
-publicUserRouter.delete('/:id',authenticate, deleteUser)
+adminUserRouter.get('/', listAllUsers);
+adminUserRouter.patch('/:id/set-admin', setAdmin);
 
 export {publicUserRouter,adminUserRouter};
