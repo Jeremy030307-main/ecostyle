ce368c13c067b62f11fdf23df4d6803da8eae853
shivanshchadda <scha0241@student.monash.edu>
2024-12-26 19:05:05 +1100

    this is done

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { CartProvider } from './CartContext.js'; // Import CartProvider
 
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Customer from './customer/customer';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -15,23 +15,25 @@ import Account from './customer/Pages/Account';
 import Product from './customer/Pages/Product';
 import Home from './customer/Pages/Home';
 import LoginSignUp from './customer/Pages/LoginSignUp';
+import Contact from './customer/Pages/Contact';
+import Footer from './customer/Pages/Footer';
+import NotFound from './customer/Pages/NotFound';
 
 import Admin from './admin/admin';
 import Add from './admin/Pages/Add';
 import Products from './admin/Pages/Products';
 import Orders from './admin/Pages/Orders';
-import Contact from './customer/Pages/Contact';
-import Footer from './customer/Pages/Footer';
-import NotFound from './customer/Pages/NotFound';
 
 import AdminRoutes from './authentication/protectedRoute';
 import Checkout from './customer/Pages/Checkout';
-import { AuthProvider } from './authentication/authContext';
 
 function App() {
   return (
     <CartProvider>  {/* Wrap the entire app in CartProvider */}
       <BrowserRouter>
+        <AuthProvider>
+          <MainApp />
+        </AuthProvider>
           <Routes>
             <Route path='/' element={<Customer/>}>
               <Route path='' element={<Home/>}></Route>
@@ -50,11 +52,43 @@ function App() {
                {/* Your admin routes */}
             </Route>
           </Routes>
-        <AuthProvider>
-          <MainApp />
-        </AuthProvider>
       </BrowserRouter>
     </CartProvider>
+  );
+}
+
+function MainApp() {
+  const location = useLocation();
+
+  // List of paths where the footer should not appear
+  const noFooterPaths = ['/admin', '/login'];
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Customer />}>
+          <Route path='' element={<Home />} />
+          <Route path='shop' element={<Shop />} />
+          <Route path='eco-point' element={<EcoPoint />} />
+          <Route path='about' element={<About />} />
+          <Route path='wishlist' element={<Wishlist />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='account' element={<Account />} />
+          <Route path='product' element={<Product />} />
+          <Route path='login' element={<LoginSignUp />} />
+          <Route path='contact' element={<Contact />} />
+        </Route>
+
+        <Route element={<AdminRoutes />}>
+          <Route path='/admin' element={<Admin />} />
+        </Route>
+
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+
+      {/* Conditional Footer */}
+      {!noFooterPaths.includes(location.pathname) && <Footer />}
+    </>
   );
 }
 
