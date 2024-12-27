f868fd7fb52fc9c46ee3b849853952df4f3e8b62
ngan0012 <your.email@example.com>
2024-12-27 15:17:28 +0530

    Resolve JSON import issue in ESM setup

--- server/firebase.js
+++ server/firebase.js
@@ -1,6 +1,13 @@
 // firebaseAdmin.js
 import admin from 'firebase-admin';
-import serviceAccount from './ecostyle-f6ae5-firebase-adminsdk-usabu-0b1c2c17bb.json' assert { type: 'json' };
+//import serviceAccount from './ecostyle-f6ae5-firebase-adminsdk-usabu-0b1c2c17bb.json' assert { type: 'json' };
+import fs from 'fs';
+
+// Load the JSON file using fs since `assert { type: 'json' }` may cause compatibility issues.
+const serviceAccount = JSON.parse(
+  fs.readFileSync('./ecostyle-f6ae5-firebase-adminsdk-usabu-0b1c2c17bb.json', 'utf8')
+);
+
 
 // Initialize Firebase Admin app if it's not already initialized
 if (!admin.apps.length) {
