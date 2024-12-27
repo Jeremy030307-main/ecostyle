94fff1337ea1904c53bd62b66a011a1a5dae49b9
ngan0012 <your.email@example.com>
2024-12-27 11:03:55 +0530

    removed the imports for admin

--- client/src/App.js
+++ client/src/App.js
@@ -13,11 +13,6 @@ import Product from './customer/Pages/Product';
 import Home from './customer/Pages/Home';
 import LoginSignUp from './customer/Pages/LoginSignUp';
 
-import Admin from './admin/admin';
-import Add from './admin/Pages/Add';
-import Products from './admin/Pages/Products';
-import Orders from './admin/Pages/Orders';
-
 import AdminRoutes from './authentication/protectedRoute';
 import Checkout from './customer/Pages/Checkout';
 
