63052600aac425ccfc1b5878cf3933e62cdd1e19
Bryan Hii <bhii0003@student.monash.edu>
2025-01-01 22:16:03 +0800

    fixed logout authentication

--- client/src/admin/Components/Navbar/Navbar.jsx
+++ client/src/admin/Components/Navbar/Navbar.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import {assets} from '../Assets/assets'
 import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import AuthenticationManager from '../../../authentication/authenticationManager';
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        // Show confirmation dialog
+        const confirmed = window.confirm("Are you sure you want to log out?");
+        
+        if (confirmed) {
+            const success = await AuthenticationManager.signOut();
+            if (success) {
+                // Redirect to login page after successful logout
+                navigate('/login');
+            }
+        }
+    };
     return (
         <div className='navbar-container'>
             <img src={assets.logo_text} alt ="" />
             <Link to='../Login'>
-                <button className='logout-btn'>Logout</button>
+                <button onClick={handleLogout} className='logout-btn'>Logout</button>
             </Link>
         </div>
     )
